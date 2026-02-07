e436fb95a0ab70fc6c18998a751af592bf92b811
Simon Shain <sashain6@gmail.com>
2026-02-06 22:10:50 -0800

    update

--- functions/signup.js
+++ functions/signup.js
@@ -51,7 +51,7 @@ export async function onRequest(context) {
     // Save the User
     await env.LIKES_STORAGE.put(userKey, JSON.stringify(userData));
 
-    await env.LIKES_STORAGE.put(limitKey, "true", { expirationTtl: 1 });
+    await env.LIKES_STORAGE.put(limitKey, "true", { expirationTtl: 60 });
 
     return new Response(JSON.stringify({ success: true, message: "Account created!" }), {
       status: 201,
