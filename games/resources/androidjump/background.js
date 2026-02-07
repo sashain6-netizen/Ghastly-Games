dd0e7d82e6e79518ef4755fb145953c3fcedfabf
Simon Shain <sashain6@gmail.com>
2026-02-06 20:46:02 -0800

    update

--- games/resources/androidjump/background.js
+++ games/resources/androidjump/background.js
@@ -26,7 +26,31 @@ export class Background {
     }
 
     draw(context) {
-        context.drawImage(this.image, this.x, this.y, this.width, this.height)
-        context.drawImage(this.image, this.x, this.y - this.height, this.width, this.height)
-    }
+    context.save();
+
+    // Create a vertical gradient: Pitch black at the bottom, dark blood-red at the top
+    // This creates the illusion of climbing out of a dark abyss
+    let gradient = context.createLinearGradient(0, 0, 0, this.height);
+    gradient.addColorStop(0, '#1a0000'); // Top: Dark Crimson
+    gradient.addColorStop(1, '#000000'); // Bottom: Pure Black
+    
+    context.fillStyle = gradient;
+    
+    // We draw two rectangles to simulate the infinite scrolling, 
+    // just like you did with the images!
+    context.fillRect(this.x, this.y, this.width, this.height);
+    context.fillRect(this.x, this.y - this.height, this.width, this.height);
+
+    // Optional: Add a "Vignette" effect (darker edges) to make it extra creepy
+    let vignette = context.createRadialGradient(
+        this.width / 2, this.height / 2, 0, 
+        this.width / 2, this.height / 2, this.width
+    );
+    vignette.addColorStop(0, 'rgba(0,0,0,0)');
+    vignette.addColorStop(1, 'rgba(0,0,0,0.8)');
+    context.fillStyle = vignette;
+    context.fillRect(0, 0, this.width, this.height);
+
+    context.restore();
+}
 }
